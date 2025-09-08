8dd4f1bc79ec7588add4ff0dbbaf9da4df7c75b7
Alexandro Antonio <alexantg47@gmail.com>
2025-09-07 19:50:00 -0600

    ADD: servicio de truck

---
+++ app/Services/TruckService.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Services;
+
+use App\Interfaces\TruckServiceInterface;
+use App\Models\Truck;
+use MongoDB\BSON\ObjectId;
+
+class TruckService implements TruckServiceInterface
+{
+    public function getAll()
+    {
+        return Truck::raw(function($collection) {
+            return $collection->aggregate([
+                [
+                    '$lookup' => [
+                        'from' => 'users',
+                        'localField' => 'user',
+                        'foreignField' => '_id',
+                        'as' => 'owner'
+                    ]
+                ],
+                ['$unwind' => '$owner']
+            ]);
+        });
+    }
+
+    public function create(array $data)
+    {
+        if (isset($data['user']) && is_string($data['user'])) {
+            $data['user'] = new ObjectId($data['user']);
+        }
+
+        return Truck::create($data);
+    }
+
+    public function find(string $id)
+    {
+        return Truck::find($id);
+    }
+
+    public function update(string $id, array $data)
+    {
+        $truck = Truck::find($id);
+        if (!$truck) return null;
+
+        if (isset($data['user']) && is_string($data['user'])) {
+            $data['user'] = new ObjectId($data['user']);
+        }
+
+        $truck->update($data);
+        return $truck;
+    }
+
+    public function delete(string $id)
+    {
+        $truck = Truck::find($id);
+        if (!$truck) return false;
+
+        $truck->delete();
+        return true;
+    }
+}
