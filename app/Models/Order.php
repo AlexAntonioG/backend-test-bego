cf0cf3abbf8f527d7c6a0f554436d3e94c954184
Alexandro Antonio <alexantg47@gmail.com>
2025-09-07 21:50:31 -0600

    ADD: modelo order

---
+++ app/Models/Order.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model as Eloquent;
+
+class Order extends Eloquent
+{
+    protected $connection = 'mongodb';
+    protected $collection = 'orders';
+
+    protected $fillable = [
+        'user',
+        'truck',
+        'status',
+        'pickup',
+        'dropoff',
+    ];
+
+    const STATUS_CREATED   = 'created';
+    const STATUS_IN_TRANSIT = 'in transit';
+    const STATUS_COMPLETED  = 'completed';
+
+    public static array $statuses = [
+        self::STATUS_CREATED,
+        self::STATUS_IN_TRANSIT,
+        self::STATUS_COMPLETED,
+    ];
+}
