2408c697258fdd0d08fefeeddf8b8a4444604b6b
Alexandro Antonio <alexantg47@gmail.com>
2025-09-07 20:39:20 -0600

    FIX: cambio de uso de servicio a interfaz en controlador de User

--- app/Interfaces/UserServiceInterface.php
+++ app/Interfaces/UserServiceInterface.php
@@ -6,4 +6,9 @@ interface UserServiceInterface
 {
     public function register(array $data);
     public function login(array $credentialsData);
+    public function getAll();
+    public function create(array $data);
+    public function find(string $id);
+    public function update(string $id, array $data);
+    public function delete(string $id);
 }
